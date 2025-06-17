ccbb7704227074fb060bb2c1ea0b81718760e521
Mauricio Souza <mauricio.salso@hotmail.com>
2025-06-17 05:38:49 -0300

    fix: Atualiza consultas SQL para utilizar a tabela Usuario em vez de Pessoa

--- Administrador/Repositories/SqlServer/Queries/UsuarioSqlServerQuery.cs
+++ Administrador/Repositories/SqlServer/Queries/UsuarioSqlServerQuery.cs
@@ -2,10 +2,10 @@ namespace MauricioGym.Administrador.Repositories.SqlServer.Queries
 {
     public static class UsuarioSqlServerQuery
     {
-        public static string InserirUsuario => "INSERT INTO Pessoa (Nome, Email, Telefone, DataNascimento, Ativo, DataCriacao) VALUES (@Nome, @Email, @Telefone, @DataNascimento, 1, GETDATE()); SELECT CAST(SCOPE_IDENTITY() as int);";
-        public static string ObterPorId => "SELECT * FROM Pessoa WHERE Id = @Id AND Ativo = 1";
-        public static string Listar => "SELECT * FROM Pessoa WHERE Ativo = 1";
-        public static string Atualizar => "UPDATE Pessoa SET Nome = @Nome, Email = @Email, Telefone = @Telefone, DataNascimento = @DataNascimento, DataAtualizacao = GETDATE() WHERE Id = @Id AND Ativo = 1";
-        public static string RemoverLogico => "UPDATE Pessoa SET Ativo = 0, DataAtualizacao = GETDATE() WHERE Id = @Id AND Ativo = 1";
+        public static string InserirUsuario => "INSERT INTO Usuario (Nome, Email, Telefone, DataNascimento, Ativo, DataCriacao) VALUES (@Nome, @Email, @Telefone, @DataNascimento, 1, GETDATE()); SELECT CAST(SCOPE_IDENTITY() as int);";
+        public static string ObterPorId => "SELECT * FROM Usuario WHERE Id = @Id AND Ativo = 1";
+        public static string Listar => "SELECT * FROM Usuario WHERE Ativo = 1";
+        public static string Atualizar => "UPDATE Usuario SET Nome = @Nome, Email = @Email, Telefone = @Telefone, DataNascimento = @DataNascimento, DataAtualizacao = GETDATE() WHERE Id = @Id AND Ativo = 1";
+        public static string RemoverLogico => "UPDATE Usuario SET Ativo = 0, DataAtualizacao = GETDATE() WHERE Id = @Id AND Ativo = 1";
     }
 }
