d2501a60ce0cae2c234d50ee5d65b8ae218d6a10
Mauricio Souza <mauricio.salso@hotmail.com>
2025-06-17 00:57:19 -0300

    feat: configurar Swagger e adicionar launchSettings para desenvolvimento

--- MauricioGym.Api/Program.cs
+++ MauricioGym.Api/Program.cs
@@ -13,11 +13,12 @@ builder.Services.AdicionarServicosMauricioGym(connectionString);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "MauricioGym API v1");
+    c.RoutePrefix = "swagger";
+});
 
 app.UseHttpsRedirection();
 
