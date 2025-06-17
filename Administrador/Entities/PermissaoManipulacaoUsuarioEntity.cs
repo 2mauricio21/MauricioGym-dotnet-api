5ae1359f1fc0eeea18eda024a03767212b19a490
Mauricio Souza <mauricio.salso@hotmail.com>
2025-06-17 07:08:29 -0300

    refactor: Atualiza entidades e repositórios para incluir novos campos e melhorar a lógica de permissões e mensalidades

--- Administrador/Entities/PermissaoManipulacaoUsuarioEntity.cs
+++ Administrador/Entities/PermissaoManipulacaoUsuarioEntity.cs
@@ -6,9 +6,10 @@ namespace MauricioGym.Administrador.Entities
     {
         public int Id { get; set; }
         public int AdministradorId { get; set; }
-        public int UsuarioId { get; set; } // Renomeado de PessoaId para UsuarioId
-        public bool PodeEditar { get; set; }
-        public bool PodeExcluir { get; set; }
+        public int UsuarioId { get; set; }
+        public string TipoPermissao { get; set; } = string.Empty; // Cadastrar, Editar, Excluir
+        public bool Ativo { get; set; } = true;
         public DateTime DataCriacao { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
     }
 }
